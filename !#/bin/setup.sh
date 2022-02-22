ccf9e72f4154c22a24c37e2bcf0f21f3c8b7ed15
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-02-22 03:19:43 +0300

    git shortcut bash file

---
+++ !#/bin/setup.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+alias jj="git add"
+alias kk="git commit -m"
+alias kl="git push origin master"
+alias gg="gcc -Wall -pedantic -Werror -Wextra"
+alias em="emacs"
+alias bt="betty"
+alias ch="chmod a+x"
+git config --global credential.helper "cache --timeout=86400"
