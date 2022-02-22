cf5621cec8d9b24c8cebe16cb64c65860676db13
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-02-22 03:17:51 +0300

    Reformatting code - betty standards

---
+++ sdl2.c
@@ -0,0 +1,85 @@
+#include "demo.h"
+
+int init_instance(SDL_Instance *instance)
+{
+	/* Initialize SDL */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		return (1);
+	}
+	/* Create a new Window instance */
+	instance->window = SDL_CreateWindow("SDL2 \\o/", SDL_WINDOWPOS_CENTERED,
+					    SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
+	if (instance->window == NULL)
+	{
+		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+		SDL_Quit();
+		return (1);
+	}
+	/* Create a new Renderer instance linked to the window */
+	instance->renderer = SDL_CreateRenderer(instance->window, -1,
+						SDL_RENDERER_ACCELERATED |
+						SDL_RENDERER_PRESENTVSYNC);
+	if (instance->renderer == NULL)
+	{
+		SDL_DestroyWindow(instance->window);
+		fprintf(stderr, "SLD_CreateRenderer Error: %s\n", SDL_GetError());
+		SDL_Quit();
+		return (1);
+	}
+	return (0);
+}
+
+void draw_stuff(SDL_Instance instance)
+{
+	SDL_SetRenderDrawColor(instance.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderDrawLine(instance.renderer, 10, 10, 100, 100);
+}
+
+int poll_events()
+{
+	SDL_Event event;
+	SDL_KeyboardEvent key;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			return (1);
+		case SDL_KEYDOWN:
+			key = event.key;
+			/* If 'ESCAPE' is pressed */
+			if (key.keysym.scancode == 0x29)
+				return (1);
+			break;
+		}
+	}
+	return (0);
+}
+
+int main(void)
+{
+	SDL_Instance instance;
+
+	if (init_instance(&instance) != 0)
+		return (1);
+	/*
+	 * C will always be awesome
+	 * This is in infinite loop
+	 */
+	while ("C is awesome")
+	{
+		SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
+		SDL_RenderClear(instance.renderer);
+		if (poll_events() == 1)
+			break;
+		draw_stuff(instance);
+		SDL_RenderPresent(instance.renderer);
+	}
+	SDL_DestroyRenderer(instance.renderer);
+	SDL_DestroyWindow(instance.window);
+	SDL_Quit();
+	return (0);
+}
